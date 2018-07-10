4c27c1b74cb5984173753646afa11bb75f4fbf2a
GaganBhat <gaganbhat6@gmail.com>
2018-07-10 12:16:58 -0500

    Encoding and Decoding bug squashed

--- InterlinkCommunications.py
+++ InterlinkCommunications.py
@@ -4,21 +4,19 @@ import socket
 
 TCP_IP = '192.168.1.6'
 TCP_PORT = 5005
-BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
 conn, addr = s.accept()
-print
-'Connection address:', addr
+print('Connection address:', addr)
 while 1:
-    data = conn.recv(BUFFER_SIZE)
+    data = conn.recv(BUFFER_SIZE).decode()
     if not data: break
     print("received data:", data)
-    conn.send(data)  # echo
-    conn.close()
+conn.close()
 #
 # ser = serial.Serial(port='COM10', baudrate='9600')
 # if ser.is_open:
